12545c099f5d338805595ad5e906dd834b5248bb
Joshua LaGorce <joshlagorce@gmail.com>
2019-10-31 15:24:12 -0600

    added jumbo componeponent and styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import React from 'react';
 import Header from './components/Header'
 import './components/Header/style.css'
-// import './App.css';
+import Jumbo from './components/Jumbo'
+import './components/Jumbo/style.css'
+import './App.css';
 
 function App() {
   return (
+    <div>
     <Header />
+    <Jumbo />
+    </div>
   );
 }
 
---
+++ src/components/Jumbo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Jumbo = (props) => {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-3">Clicky Game!</h1>
+                <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Jumbo;
