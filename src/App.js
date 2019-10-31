0f1af01e968ef4cb1cee2f8228c9412669325ada
Joshua LaGorce <joshlagorce@gmail.com>
2019-10-31 15:40:51 -0600

    added image componeonent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-// import React, { Component } from "react";
+
 import React from 'react';
 import Header from './components/Header'
-import './components/Header/style.css'
 import Jumbo from './components/Jumbo'
-import './components/Jumbo/style.css'
+import Container from './components/Container'
+import GameImg from './components/GameImg'
 import './App.css';
 
 function App() {
   return (
-    <div>
-    <Header />
-    <Jumbo />
-    </div>
+    <Container>
+      <Header />
+      <Jumbo />
+      <GameImg />
+    
+   </Container>
   );
 }
 
---
+++ src/components/GameImg/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css'
+
+const Gameimg = (props) => {
+    return (
+        <div>
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt=""></img>
+                
+            </div>
+
+        </div>
+        
+        
+    );
+};
+
+export default Gameimg;
